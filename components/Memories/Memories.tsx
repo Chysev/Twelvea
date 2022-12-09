0c921c60b0d8c3346d4eb5cf41d521476435e124
Chysev <Johnlayda92@gmail.com>
2022-12-09 23:40:02 +0400

    video removed from Public and uploaded it on GDrive

--- components/Memories/Memories.tsx
+++ components/Memories/Memories.tsx
@@ -1,3 +1,5 @@
+import Slider from 'react-slick'
+
 // Component
 import Template from "../Template/Template"
 import Code404 from "../404/404"
@@ -5,7 +7,7 @@ import Code404 from "../404/404"
 const Memories = () => {
 
     const settings = {
-        className: "flex max-h-[600px] max-w-[1050px]  border-0 m-auto",
+        className: "m-auto w-[65%] py-[2.5rem] 1024max:w-[80%] grid gap-[.3rem] pt-[5rem]",
         dots: false,
         autoplay: true,
         speed: 1000,
@@ -25,7 +27,59 @@ const Memories = () => {
     }
 
     return (
-        <div></div>
+        <div className="flex min-h-[100vh] h-[100%] 992max:px-[2rem] BG">
+            <Slider {...settings}>
+
+                <div>
+                    <iframe
+                        className=" h-[auto] w-[100%]  aspect-video  rounded-[17.5px]  active:mb-[3rem] hover: transition-all"
+                        src="https://drive.google.com/file/d/1IBAfuwm793UWwNj8h9sU-o5WJpGzUSu6/preview"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
+                </div>
+
+                <div>
+                    <iframe
+                        className=" h-[auto] w-[100%]  aspect-video  rounded-[17.5px]  active:mb-[3rem] hover: transition-all"
+                        src="https://drive.google.com/file/d/1wDM4LORk6Pp0D_SpmBeCLwwU8-YwICZT/preview"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
+                </div>
+
+                <div>
+                    <iframe
+                        className=" h-[auto] w-[100%]  aspect-video  rounded-[17.5px]  active:mb-[3rem] hover: transition-all"
+                        src="https://drive.google.com/file/d/1GlhEql7j2arlLoNFgRuMrWdHvz4APwdx/preview"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
+                </div>
+
+                <div>
+                    <iframe
+                        className=" h-[auto] w-[100%]  aspect-video  rounded-[17.5px]  active:mb-[3rem] hover: transition-all"
+                        src="https://drive.google.com/file/d/12-hIYNL17phoFIf-EwpSbI_DJsKWMyv2/preview"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
+                </div>
+
+                <div>
+                    <iframe
+                        className=" h-[auto] w-[100%]  aspect-video  rounded-[17.5px]  active:mb-[3rem] hover: transition-all"
+                        src="https://drive.google.com/file/d/1TH4wIJhnl2N5P7JsHxzqeaaY-sXRgKRS/preview"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
+                </div>
+
+                <div>
+                    <iframe
+                        className=" h-[auto] w-[100%]  aspect-video  rounded-[17.5px]  active:mb-[3rem] hover: transition-all"
+                        src="https://drive.google.com/file/d/1cNveh-k41ilJfyolyVDbSQOPXXX5sIWn/preview"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
+                </div>
+            </Slider>
+        </div>
+
     )
 }
 export default Memories
--- components/Template/Template.tsx
+++ components/Template/Template.tsx
@@ -1,7 +1,7 @@
 const Template = ({ children }) => {
     return (
         <div className="flex min-h-[100vh] h-[100%]">
-            <div className="px-[20px] justify-center flex max-w-[1200px] w-[100%] m-auto About DevNote Memories Tropa">
+            <div className="px-[20px] justify-center flex max-w-[1200px]  w-[100%] m-auto About DevNote Memories Tropa">
                 {children}
             </div>
         </div>
